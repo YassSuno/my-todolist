7570e8f146975d543a1054224459210c81c7e69d
Sun <78863990+YassSuno@users.noreply.github.com>
2024-09-05 22:28:48 +0700

    Update ToDoList.tsx

--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import "../App.css";
 import { ToDoCard } from "./ToDoCard";
+import { SetOfToDoList } from "../App";
 
 export interface ToDoListProps {
-  toDoList: string[];
-  setToDoList: React.Dispatch<React.SetStateAction<string[]>>;
+  toDoList: SetOfToDoList[];
+  setToDoList: React.Dispatch<React.SetStateAction<SetOfToDoList[]>>;
 }
 
 export function ToDoList({ toDoList, setToDoList }: ToDoListProps) {
+  // console.log("12345", toDoList);
   return (
     <div className="container">
       {/* show by .map */}
       {toDoList.map((value, index) => {
         return (
           <ToDoCard
+            key={index}
             value={value}
             index={index}
             setToDoList={setToDoList}
