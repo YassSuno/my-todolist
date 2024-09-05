2eb59a58393a4568485eaaf44e32de3ee21070ba
Sun <78863990+YassSuno@users.noreply.github.com>
2024-09-05 22:27:14 +0700

    Update InputButton.tsx

--- src/component/InputButton.tsx
+++ src/component/InputButton.tsx
@@ -1,27 +1,67 @@
 import React from "react";
 import "../App.css";
+import { SetOfToDoList } from "../App";
 
 export interface InputButtonProps {
-  toDoList: string[];
-  setToDoList: React.Dispatch<React.SetStateAction<string[]>>;
+  toDoList: SetOfToDoList[];
+  setToDoList: React.Dispatch<React.SetStateAction<SetOfToDoList[]>>;
 }
+
+export interface NewSetOfToDoList {
+  isChecked: boolean;
+  name: string;
+  isEditing: boolean;
+  isDeleted: boolean;
+}
+
 export function InputButton({ toDoList, setToDoList }: InputButtonProps) {
   const [query, setQuery] = React.useState("");
 
-  const getInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+  const getInput = (event) => {
+    setQuery(event.target.value);
   };
 
   const addToArray = () => {
-    if (query.trim() !== "") {
-      setToDoList([...toDoList, query]);
-      setQuery("");
-    }
+    setToDoList([
+      ...toDoList,
+      {
+        isChecked: false,
+        name: query,
+        isEditing: false,
+        isDeleted: false,
+      },
+    ]);
+    setQuery("");
   };
+  // const [query, setQuery] = React.useState<NewSetOfToDoList>({
+  //   isChecked: false,
+  //   name: "",
+  //   isEditing: false,
+  //   isDeleted: false,
+  // });
+
+  // const getInput = (event) => {
+  //   setQuery({
+  //     isChecked: false,
+  //     name: event.target.value,
+  //     isEditing: false,
+  //     isDeleted: false,
+  //   });
+  // };
+
+  // const addToArray = () => {
+  //   setToDoList([...toDoList, query]);
+  //   setQuery({
+  //     isChecked: false,
+  //     name: "",
+  //     isEditing: false,
+  //     isDeleted: false,
+  //   });
+  // };
 
   return (
     <div className="input">
-      <input value={query} onChange={getInput} />
+      <input required value={query} onChange={getInput} />
       <button onClick={addToArray}>Submit</button>
     </div>
   );
